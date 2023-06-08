b5162e77a0b000c00a4dd7e8628d979156d8d3f3
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-06-08 23:34:34 +0530

    cors origin

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express=require('express');
 const app=express();
 
 const cors=require('cors');
-app.use(cors({credentials: true, origin: 'https://insightly-frontend.onrender.com/'}));
+app.use(cors({credentials: true, origin: 'https://insightly-frontend.onrender.com'}));
 
 const connectDB=require("./config/db");
 connectDB();
